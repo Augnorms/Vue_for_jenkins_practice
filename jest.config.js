de72b1fe23b4febfbc862c5a5a31c51f0cb67eb1
Augustine Normanyo <augustinenormanyo98@gmail.com>
2025-04-12 12:29:48 +0000

    added the outsource for jest to create an xml file

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: 'test-results',
+        outputName: 'junit.xml',
+        suiteName: 'jest tests',
+        classNameTemplate: '{classname}-{title}',
+        titleTemplate: '{classname}-{title}',
+        ancestorSeparator: ' › ',
+        usePathForSuiteName: true
+      }
+    ]
+  ]
 };
